ec2e6878e34b9e170ba76896ea3e2f8924017635
Hosein <hoseinshurabi@gmail.com>
2023-10-09 14:41:13 +0330

    Monday-14-41 fix round-up

--- fabychecker/fabychecker.sh
+++ fabychecker/fabychecker.sh
@@ -6,15 +6,10 @@ if [ "$#" -ne 3 ]; then
   exit 1
 fi
 
+
 text="$1"
 number="$2"
 wordlist_file="$3"
-wordlist_len=$(wc -l < "$wordlist_file")
-
-if ((number > wordlist_len)); then
-   echo "Ah Okay, Are you shitting me!? or are you crazy???"
-   exit 1
-fi
 
 if [ ! -f "$wordlist_file" ]; then
   echo "Hey man! Are you fucking blind?! The file \"$wordlist_file\" not exist"
@@ -22,8 +17,17 @@ if [ ! -f "$wordlist_file" ]; then
 fi
 
 
+wordlist_len=$(wc -l < "$wordlist_file")
+
+if ((number < 1)) ||((number > wordlist_len)); then
+   echo "Ah Okay, Are you shitting me!? or are you crazy???"
+   exit 1
+fi
+
+
+
 normalize=$((number+1))
-sp=$((wordlist_len / normalize))
+sp=$(echo $((wordlist_len / normalize)) | awk '{printf "%.0f\n", $1 + 0.2}')
 current_line=0
 flag=1
 
