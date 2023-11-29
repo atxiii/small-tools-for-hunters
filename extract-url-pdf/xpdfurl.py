b7072e58a099c8283e0b4b861e9d012cd2f449a6
Hosein <hoseinshurabi@gmail.com>
2023-11-29 20:18:22 +0330

    Wednesday-20-18

--- extract-url-pdf/xpdfurl.py
+++ extract-url-pdf/xpdfurl.py
@@ -2,16 +2,8 @@
 import argparse
 import PyPDF2
 import re
-
-def validate_url(url):
-    # Regular expression pattern to match URLs, allowing for some non-standard characters
-    pattern = re.compile(r'^(https?|ftp)://[^\s/$.?#].[^\s]*$')
-
-    # Check if the URL matches the pattern
-    if re.match(pattern, url):
-        return True
-    else:
-        return False
+from urlextract import URLExtract
+extractor = URLExtract()
 
 def extract_urls_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as file:
@@ -21,9 +13,8 @@ def extract_urls_from_pdf(pdf_path):
         for page_num in range(num_pages):
             page = pdf_reader.pages[page_num]
             text = page.extract_text()
-            extracted_urls = re.findall(r'(?:http[s]?://)?(?:www\.)?[a-zA-Z0-9]+\.[a-zA-Z]{2,}(?:\S+)?', text)
+            extracted_urls = extractor.find_urls(text)
             urls.extend(extracted_urls)
-
         return urls
 
 def main():
@@ -33,8 +24,7 @@ def main():
 
     extracted_urls = extract_urls_from_pdf(args.file_path)
     for url in extracted_urls:
-        if validate_url(url):
-            print(url)
+        print(url)
 
 if __name__ == "__main__":
     main()
